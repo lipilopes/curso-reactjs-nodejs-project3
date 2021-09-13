9016ecb3e8b7aa8a0be8dbce3a60c72582d84013
Fellipe Lopes <felippe091234@gmail.com>
2021-09-13 19:21:27 -0300

    Global Styles

---
+++ src/styles/theme.js
@@ -0,0 +1,38 @@
+export const theme = {
+  colors: {
+    white: "white",
+    mediumGray: #DDDDDD,
+    black: "black",
+    primaryColor: "#0A1128",
+    secondaryColor: "#dc143c",
+  },
+  fonts: {
+    family: {
+      OpenSans: "Open Sans, sans-serif",
+      Montserrat: "Montserrat, 'ans-serif",
+    },
+    sizes: {
+      xsmall: "8rem",
+      small: "1.6rem",
+      medium: "2.4rem",
+      large: "3.2rem",
+      xlarge: "4.0rem",
+      big: "4.8rem",
+      huge: "5.6rem",
+      xhuge: "6.4rem",
+    },
+  },
+  spacings: {
+    xsmall: "8rem",
+    small: "1.6rem",
+    medium: "2.4rem",
+    large: "3.2rem",
+    xlarge: "4.0rem",
+    big: "4.8rem",
+    huge: "5.6rem",
+    xhuge: "6.4rem",
+  },
+  media: {
+    lteMedium: "(max-width: 768px)",
+  },
+};
