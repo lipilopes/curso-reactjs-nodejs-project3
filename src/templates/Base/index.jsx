d13aa50f62778717d631a2c05360ba10db2dc467
Fellipe Lopes <felippe091234@gmail.com>
2021-09-13 19:27:27 -0300

    Templates Base
    Files:
    * index
    * teste
    * mock
    * stories
    *syles

---
+++ src/templates/Base/index.jsx
@@ -0,0 +1,26 @@
+/* eslint-disable react/prop-types */
+import P from "prop-types";
+import { Container } from "./styles";
+import { Menu } from "../components/Menu";
+import { Footer } from "../components/Footer";
+import { GoTop } from "../../components/GoTop";
+
+export const Base = ({ links = [], logoData, footerHtml, children }) => {
+  return (
+    <>
+      <Menu links={links} logoData={logoData} />
+
+      <Container>
+        {children}
+        <Footer footerHtml={footerHtml} />
+      </Container>
+      <GoTop />
+    </>
+  );
+};
+
+Base.prototype = {
+  html: P.string.isRequired,
+  ...Menu.propTypes,
+  footerHtml: P.string.isRequired,
+};
---
+++ src/templates/Base/stories.jsx
@@ -0,0 +1,21 @@
+import { Base } from ".";
+
+import mock from "./mock";
+import { GridText } from "../../components/GridText";
+import gridMock from "../../components/GridText/mock";
+
+import mockBase from "./mock";
+
+export default {
+  title: "Templates/Base",
+  component: Base,
+  args: mockBase,
+};
+
+export const Template = (args) => {
+  return (
+    <div style={{ height: "400vh" }}>
+      <Base {...args} />
+    </div>
+  );
+};
